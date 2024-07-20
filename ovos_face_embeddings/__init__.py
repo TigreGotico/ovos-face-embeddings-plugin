36926af7038de139025b38ec502193c6b352748b
miro <jarbasai@mailfence.com>
2024-07-20 16:55:24 +0100

    auto setup chromadb

--- ovos_face_embeddings/__init__.py
+++ ovos_face_embeddings/__init__.py
@@ -1,25 +1,36 @@
+import os.path
+from typing import Optional
+
 import face_recognition
 import numpy as np
-from ovos_plugin_manager.templates.embeddings import FaceEmbeddingsStore, EmbeddingsDB
+from ovos_chromadb_embeddings import ChromaEmbeddingsDB
+from ovos_config.locations import get_xdg_cache_save_path
+from ovos_plugin_manager.templates.embeddings import EmbeddingsDB
+from ovos_plugin_manager.templates.embeddings import FaceEmbeddingsStore
+from ovos_utils.log import LOG
 
 
 class FaceEmbeddingsRecognitionPlugin(FaceEmbeddingsStore):
+    def __init__(self, db: Optional[EmbeddingsDB] = None, thresh: float = 0.75):
+        if db is None:
+            db_path = get_xdg_cache_save_path("chromadb")
+            os.makedirs(db_path, exist_ok=True)
+            db_path = f"{db_path}/face_prints"
+            LOG.info(f"Using chromadb as face embeddings store: {db_path}")
+            db = ChromaEmbeddingsDB(db_path)
+        super().__init__(db, thresh)
 
     def get_face_embeddings(self, frame: np.ndarray) -> np.ndarray:
         return face_recognition.face_encodings(frame)[0]
 
 
 if __name__ == "__main__":
-    from ovos_chromadb_embeddings import ChromaEmbeddingsDB
-    path = "/tmp/face_db"
-    db = ChromaEmbeddingsDB(path)
-
     # Example usage:
     a = "/home/miro/PycharmProjects/ovos-user-id/a1.jpg"
     a2 = "/home/miro/PycharmProjects/ovos-user-id/a2.jpg"
     b = "/home/miro/PycharmProjects/ovos-user-id/b.jpg"
 
-    f = FaceEmbeddingsRecognitionPlugin(db)
+    f = FaceEmbeddingsRecognitionPlugin()
 
     e1 = face_recognition.load_image_file(a)
     e2 = face_recognition.load_image_file(a2)
@@ -30,4 +41,3 @@ if __name__ == "__main__":
     print(f.query(e1))
     print(f.query(e2))
     print(f.query(b))
-
