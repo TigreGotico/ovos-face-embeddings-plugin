53add06ce6ace274e2c4450cc39c8338d5eac1d7
miro <jarbasai@mailfence.com>
2024-07-20 01:49:20 +0100

    docstrs

--- ovos_face_embeddings/__init__.py
+++ ovos_face_embeddings/__init__.py
@@ -1,26 +1,25 @@
 import face_recognition
 import numpy as np
-from ovos_chromadb_embeddings import ChromaEmbeddingsDB
-from ovos_plugin_manager.templates.embeddings import FaceEmbeddingsStore
+from ovos_plugin_manager.templates.embeddings import FaceEmbeddingsStore, EmbeddingsDB
 
 
 class FaceEmbeddingsRecognitionPlugin(FaceEmbeddingsStore):
-    def __init__(self, thresh: float = 0.75):
-        path = "/tmp/face_db"  # TODO
-        db = ChromaEmbeddingsDB(path)
-        super().__init__(db, thresh)
 
     def get_face_embeddings(self, frame: np.ndarray) -> np.ndarray:
         return face_recognition.face_encodings(frame)[0]
 
 
 if __name__ == "__main__":
+    from ovos_chromadb_embeddings import ChromaEmbeddingsDB
+    path = "/tmp/face_db"
+    db = ChromaEmbeddingsDB(path)
+
     # Example usage:
     a = "/home/miro/PycharmProjects/ovos-user-id/a1.jpg"
     a2 = "/home/miro/PycharmProjects/ovos-user-id/a2.jpg"
     b = "/home/miro/PycharmProjects/ovos-user-id/b.jpg"
 
-    f = FaceEmbeddingsRecognitionPlugin()
+    f = FaceEmbeddingsRecognitionPlugin(db)
 
     e1 = face_recognition.load_image_file(a)
     e2 = face_recognition.load_image_file(a2)
